0582c8cc7713789dc601978512e4a270c5b8c8c7
stainlessray <raymondcoolmail@gmail.com>
2020-10-24 18:51:13 +0000

    Working with descriptive comments

--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,32 @@
 package com.codedifferently.part01;
 
+// import java.io.Console;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Greetings {
     public static void main(String[] args) {
 
+        // Create Scanner object with reference name "scanner"
+       Scanner scanner = new Scanner(System.in);
+
+       // Print out the request for input
+       System.out.println("Input your name ");
+
+       // Wait for input and assign the input value to a variable named "name"
+       String name = scanner.next();
+
+       
+       // Conditional block to determine what output will result from the input "name"
+         // IF name is equal to hakim ignroing case OR
+         // IF name is equal to rashida ignoring case
+       if (name.equalsIgnoreCase("Hakim") || name.equalsIgnoreCase("Rashida")) {
+           System.out.printf("Welcome %s! %n", name);
+
+       // ELSE print "NON GREETING"
+       } 
+       else {
+            System.out.println("I don't even know you, " + name);
+        } 
     }
 }
