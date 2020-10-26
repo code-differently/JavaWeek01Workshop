85f4cc6db6d7f29517422f5f7840fcd0b90c0829
JM <JM@Stephanies-MBP.fios-router.home>
2020-10-26 19:07:49 -0400

    Normalize and respond to input

--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,36 @@
 package com.codedifferently.part01;
 
+import java.util.Scanner;
+
 public class Greetings {
     public static void main(String[] args) {
+        System.out.println("Please write your name ...");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next();
+        answer = normalizeString(answer);
+        if ((answer.equalsIgnoreCase("Hakim")) || (answer.equalsIgnoreCase("Rasheeda"))) {
+            System.out.printf("Hello, %s!%n", answer);
+        }
+        else {
+            System.out.println("I don't know you.");
+        }
+    }
 
+    // Make sure the answer is valid
+    private static String normalizeString(String answer) {
+        answer = answer.toLowerCase();
+
+        if (answer.length() <= 0) {
+            throw new IllegalArgumentException("The string is empty.");
+        }
+
+        char[] letters = answer.toCharArray();
+        for (char letter : letters) {
+            if (Character.isLetter(letter) == false) {
+                throw new IllegalArgumentException("One or more characters is not a letter.");            
+            }
+        }
+
+        return answer;
     }
 }
